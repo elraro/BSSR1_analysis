cab35a4a3234c11421d138023d42ab7ce74d7dfe
elraro <elraro@elraro.eu>
2016-07-15 11:28:06 +0200

    Arreglado fallos 2

--- PrintROC.py
+++ PrintROC.py
@@ -2,23 +2,19 @@ import matplotlib.pyplot as plt
 
 def normalize_list(lst):
     r = max(lst) - min(lst)
-    if (r == 0):
-        return lst
+    # if (r == 0):
+    #     return lst
     # Normalize
     normal = map(lambda x: (x - min(lst)) / r, lst)
     # print(list(normal))
     return list(normal)
 
 def printROC(FPRate, FNRate, name):
-    x = [-0.1, 1.1]
+    x = [0, 1]
     FPRate = normalize_list(FPRate)
     FNRate = normalize_list(FNRate)
 
     FPRate, FNRate = (list(x) for x in zip(*sorted(zip(FPRate, FNRate), reverse=True, key=lambda pair: pair[0])))
-    # FNRate, FPRate = (list(x) for x in zip(*sorted(zip(FNRate, FPRate), reverse=True, key=lambda pair: pair[0])))
-
-    #print(FPRate)
-    #print(FNRate)
 
     plt.figure()
     plt.plot(x, x, linestyle='dashed', color='red', linewidth=2, label='y = x')
--- XMLReader.py
+++ XMLReader.py
@@ -1,40 +1,38 @@
 import xml.etree.ElementTree as ET
 import PrintROC
 import numpy as np
-from random import randint
 
 treeEnrollees = ET.parse('/home/alberto/Desktop/bssr1/fing_x_face/sets/dos/enrollees.xml')
 treeUsers = ET.parse('/home/alberto/Desktop/bssr1/fing_x_face/sets/dos/users.xml')
 rootEnrollees = treeEnrollees.getroot()
 rootUsers = treeUsers.getroot()
 
-groundwith = -1
-
 FPRate = []
 FNRate = []
 
-for childUser in rootUsers:
+TP = 0
+TN = 0
+FP = 0
+FN = 0
 
-    groundwith += 1
+for umbral in np.arange(0, 1, 0.01):
 
-    with open('/home/alberto/Desktop/bssr1/fing_x_face/sims/dos/face/C/' + childUser.attrib['name']) as f:
-        # print(childUser.attrib['name'])
-        lines = f.readlines()
-        scores = lines[2:] # remove first 2 elements
-        scores.pop() # remove last element
-        scores = [float(score.strip('\n')) for score in scores]
+    groundwith = -1
 
-        # una vez los tengo leidos, tengo que calcular cual es TP, TN, FP, FN
-        # como? comparando con mi lista de genuine y sabiendo si es genuino de verdad o no (por la posición de
-        # la diagonal, tendré que llevar un contador)
-        # el umbral que vaya variando
+    for childUser in rootUsers:
 
-        for umbral in np.arange(0,1,0.01):
+        groundwith += 1
 
-            TP = 0
-            TN = 0
-            FP = 0
-            FN = 0
+        with open('/home/alberto/Desktop/bssr1/fing_x_face/sims/dos/face/C/' + childUser.attrib['name']) as f:
+            lines = f.readlines()
+            scores = lines[2:] # remove first 2 elements
+            scores.pop() # remove last element
+            scores = [float(score.strip('\n')) for score in scores]
+
+            # una vez los tengo leidos, tengo que calcular cual es TP, TN, FP, FN
+            # como? comparando con mi lista de genuine y sabiendo si es genuino de verdad o no (por la posición de
+            # la diagonal, tendré que llevar un contador)
+            # el umbral que vaya variando
 
             for x in range(0, len(scores)):
                 if x == groundwith: # Esta es la comparación de su propio score
@@ -48,8 +46,8 @@ for childUser in rootUsers:
                     else:
                         TN += 1
 
-            # Voy a calcular ahora los rates
-            FPRate.append(FP / (FP + TN))
-            FNRate.append(FN / (FN + TP))
+    # Voy a calcular ahora los rates
+    FPRate.append(FP / (FP + TN))
+    FNRate.append(FN / (FN + TP))
 
 PrintROC.printROC(FPRate, FNRate, "Curva ROC")
