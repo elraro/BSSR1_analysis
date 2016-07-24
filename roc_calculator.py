957f1f93b18ec730ff3929a930f7619a5faaaa35
elraro <elraro@elraro.eu>
2016-07-24 22:58:30 +0200

    Pruebas 1

--- roc_calculator.py
+++ roc_calculator.py
@@ -1,3 +1,4 @@
+import numpy as np
 import matplotlib.pyplot as plt
 
 def normalize_list(lst):
@@ -6,31 +7,46 @@ def normalize_list(lst):
     normal = map(lambda x: (x - min(lst)) / r, lst)
     return list(normal)
 
-def calculate_roc(matrix, name):
-    x = [0, 1]
-    # FNRate = normalize_list(FNRate)
-    # FPRate = normalize_list(FPRate)
+def calculate_roc(matrix):
+    identity = np.identity(517)
+    TPRate = np.empty(shape=0)
+    TNRate = np.empty(shape=0)
+    FNRate = np.empty(shape=0)
+    FPRate = np.empty(shape=0)
+    for umbral in np.arange(0, 1, 0.01):
+        TP = np.logical_and(umbral > matrix, identity == 1).sum()
+        TN = np.logical_and(umbral <= matrix, identity == 0).sum()
+        FP = np.logical_and(umbral > matrix, identity == 0).sum()
+        FN = np.logical_and(umbral <= matrix, identity == 1).sum()
+        TPRate = np.append(TPRate, TP / (TP + FN))
+        TNRate = np.append(TNRate, TN / (TN + FP))
+        FNRate = np.append(FNRate, FN / (FN + + TP))
+        FPRate = np.append(FPRate, FP / (FP + TN))
+    # print(TP)
+    # print(TN)
+    # print(FP)
+    # print(FN)
 
-    # FPRate, FNRate = (list(x) for x in zip(*sorted(zip(FPRate, FNRate), reverse=True, key=lambda pair: pair[0])))
-
+    print(FNRate)
+    print(FPRate)
     plt.figure()
-    plt.plot(x, x, linestyle='dashed', color='red', linewidth=2, label='y = x')
-    plt.plot(fn_rate, fp_rate, linewidth=1, color='blue', alpha=0.5)
-
-    dif = 1
-    index = 0
-    for y in range(0, len(fp_rate)):
-        if abs(fp_rate[y] - fn_rate[y]) < dif:
-            dif = abs(fp_rate[y] - fn_rate[y])
-            index = y
-
-    print("eer: " + str((fp_rate[index] + fn_rate[index])/2))
-    print("index: " + str(index))
-    plt.axvline(x=(fn_rate[index] + fn_rate[index+1])/2, ymin=0, ymax=(fp_rate[index] + fp_rate[index+1])/2, color='black', linewidth=2, label='EER = ' + str((fn_rate[index] + fn_rate[index+1])/2))
+    plt.plot(FNRate[::-1], FPRate[::-1], linewidth=1, color='blue', alpha=0.5)
+    # dif = 1
+    # index = 0
+    # for y in range(0, len(FPRate)):
+    #     print(str(abs(FPRate[y] - FNRate[y])))
+    # print(index)
+    # if abs(FPRate[y] - FNRate[y]) < dif:
+    #     dif = abs(FPRate[y] - FNRate[y])
+    # index = y
+    # print("eer: " + str((FPRate[index] + FNRate[index]) / 2))
+    # print("index: " + str(index))
+    # plt.axvline(x=(FNRate[index] + FNRate[index + 1]) / 2, ymin=0, ymax=(FPRate[index] + FPRate[index + 1]) / 2,
+    #             color='black', linewidth=2, label='EER = ' + str((FNRate[index] + FNRate[index + 1]) / 2))
     plt.xlabel('False Negative Rate')
     plt.ylabel('False Positive Rate')
-    plt.title(name)
+    plt.title("roc")
     plt.legend(loc="lower right")
-    # plt.show()
-    plt.savefig('/home/alberto/Desktop/test/' + name + '.png')
+    plt.show()
+    # plt.savefig('/home/alberto/Desktop/test/' + name + '.png')
     plt.close()
