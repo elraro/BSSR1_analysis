1e4179716d859a37a891eccc2720ea818c077625
elraro <elraro@elraro.eu>
2016-09-14 11:56:51 +0200

    Añadido medición de tiempo

--- fing_x_face.py
+++ fing_x_face.py
@@ -2,7 +2,10 @@ import matrix_generator as mg
 import matrix_fusion as mf
 import roc_calculator as rc
 from multiprocessing import Pool
+from datetime import datetime
 
+
+startTime = datetime.now()
 
 matrix_face_c = mg.matrix_generator("/home/alberto/Desktop/bssr1/fing_x_face/sets/dos/users.xml",
                                     "/home/alberto/Desktop/bssr1/fing_x_face/sims/dos/face/C/", True, "face c")
@@ -19,6 +22,7 @@ matrix_fusion_list_face_c_li_v = mf.matrix_fusion(matrix_face_c, matrix_li_v)
 matrix_fusion_list_face_c_ri_v = mf.matrix_fusion(matrix_face_c, matrix_ri_v)
 matrix_fusion_list_face_g_li_v = mf.matrix_fusion(matrix_face_g, matrix_li_v)
 matrix_fusion_list_face_g_ri_v = mf.matrix_fusion(matrix_face_g, matrix_ri_v)
+matrix_fusion_list_li_v_ri_v = mf.matrix_fusion(matrix_ri_v, matrix_ri_v)
 
 # Dibujar curva ROC
 matrix_list = list()
@@ -33,6 +37,7 @@ err_values_face_c_li_v = pool.map(rc.calculate_roc, matrix_fusion_list_face_c_li
 err_values_face_c_ri_v = pool.map(rc.calculate_roc, matrix_fusion_list_face_c_ri_v)
 err_values_face_g_li_v = pool.map(rc.calculate_roc, matrix_fusion_list_face_g_li_v)
 err_values_face_g_ri_v = pool.map(rc.calculate_roc, matrix_fusion_list_face_g_ri_v)
+err_values_li_v_ri_v = pool.map(rc.calculate_roc, matrix_fusion_list_li_v_ri_v)
 roc_values = pool.map(rc.calculate_roc, matrix_list)
 pool.close()
 pool.join()
@@ -42,4 +47,7 @@ rc.draw_roc_eer(err_values_face_c_li_v, "face c, li v", roc_values[0]["eer"], ro
 rc.draw_roc_eer(err_values_face_c_ri_v, "face c, ri v", roc_values[0]["eer"], roc_values[3]["eer"])
 rc.draw_roc_eer(err_values_face_g_li_v, "face g, li v", roc_values[1]["eer"], roc_values[2]["eer"])
 rc.draw_roc_eer(err_values_face_g_ri_v, "face g, ri v", roc_values[1]["eer"], roc_values[3]["eer"])
-# rc.draw_roc(roc_values, "fing_x_face")
+rc.draw_roc_eer(err_values_li_v_ri_v, "li v, ri v", roc_values[2]["eer"], roc_values[3]["eer"])
+rc.draw_roc(roc_values, "fing_x_face")
+
+print("Executed in: " + str(datetime.now() - startTime))
