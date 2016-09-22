8a3f74ebc89b639e07ed618422f2a6da183b9542
elraro <elraro@elraro.eu>
2016-09-22 11:33:15 +0200

    Fusion con grupos de train y test

--- matrix_fusion.py
+++ matrix_fusion.py
@@ -7,7 +7,21 @@ def matrix_fusion(matrix_1_dict, matrix_2_dict):
     matrix_2 = matrix_2_dict["matrix"]
     for alpha in np.arange(0, 1.01, 0.01):
         matrix_list.append(matrix_fusion_alpha(matrix_1, matrix_2, alpha))
-    return matrix_list
+    matrix_list_train = list()
+    matrix_1_train = matrix_1_dict["matrix_train"]
+    matrix_2_train = matrix_2_dict["matrix_train"]
+    for alpha in np.arange(0, 1.01, 0.01):
+        matrix_list_train.append(matrix_fusion_alpha(matrix_1_train, matrix_2_train, alpha))
+    matrix_list_test = list()
+    matrix_1_test = matrix_1_dict["matrix_test"]
+    matrix_2_test = matrix_2_dict["matrix_test"]
+    for alpha in np.arange(0, 1.01, 0.01):
+        matrix_list_test.append(matrix_fusion_alpha(matrix_1_test, matrix_2_test, alpha))
+    matrix_total = dict()
+    matrix_total["matrix"] = matrix_list
+    matrix_total["matrix_train"] = matrix_list_train
+    matrix_total["matrix_test"] = matrix_list_test
+    return matrix_total
 
 
 def matrix_fusion_alpha(matrix_1, matrix_2, alpha):
--- matrix_generator.py
+++ matrix_generator.py
@@ -1,14 +1,13 @@
-import xml.etree.ElementTree as e_t
 import numpy as np
-from sklearn.cross_validation import train_test_split
 
 
-def partition_matrix(matrix):
+def partition_matrix(matrix, percent_test):
     # Train and test partitions
-    y = range(517)
-    matrix_train, matrix_test, list_train, list_test = train_test_split(matrix, y, test_size=0.33, random_state=42)
-    #return normalized_matrix, normalized_matrix2
-    return "asd"
+    total = len(matrix)
+    train = int(total * percent_test)
+    matrix_train = matrix[0:train,0:train]
+    matrix_test = matrix[train:total,train:total]
+    return matrix_train, matrix_test
 
 
 def matrix_generator(file, normalize, name):
@@ -16,11 +15,11 @@ def matrix_generator(file, normalize, name):
     if normalize:
         matrix = normalize_matrix(matrix)
     r_matrix = dict()
-    r_matrix["matrix_total"] = matrix # np.matrix(matrix)
-    training, test = partition_matrix(r_matrix["matrix_total"])
+    r_matrix["matrix"] = matrix
+    matrix_train, matrix_test = partition_matrix(r_matrix["matrix"], 0.33)
     r_matrix["aux"] = name
-    r_matrix["matrix"] = training
-    r_matrix["test"] = test
+    r_matrix["matrix_train"] = matrix_train
+    r_matrix["matrix_test"] = matrix_test
     return r_matrix
 
 
